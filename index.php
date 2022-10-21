9f36562cdb399f72a8dd7b84f858e1d6bbafc407
Cécile Both <cecile.both@swissonline.ch>
2022-10-21 11:04:30 +0200

    fixed css

--- index.php
+++ index.php
@@ -13,6 +13,8 @@
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.0/css/all.min.css" integrity="sha512-xh6O/CkQoPOWDdYTDqeRdPCVd1SpvCA9XXcUnZS2FmJNp1coAFzvtCN9BmamE+4aHK8yyUHUSCcJHgXloTyT2A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <script src="https://kit.fontawesome.com/15181efa86.js" crossorigin="anonymous"></script>
   <link rel="stylesheet" href="https://unpkg.com/bulma@0.9.0/css/bulma.min.css" />
+  <link rel="stylesheet" href="css/recipe-postits.css">
+  <link rel="stylesheet" href="css/accordion.css">
   <link rel="stylesheet" href="css/style.css">
   <!-- <link rel="stylesheet" type="text/css" href="../css/register.css"> -->
 
